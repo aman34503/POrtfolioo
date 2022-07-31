c81ca7a744e02487432032b36ac1e06d007ad71f
aman <aman34503@gmail.com>
2022-07-31 14:07:48 +0530

    done with firebase and icon image

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import firebase from "firebase";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyC4i71e-FleVQht1CJm01q2MY7HbFA8kr0",
   authDomain: "portfolio-65001.firebaseapp.com",
   projectId: "portfolio-65001",
@@ -13,12 +11,9 @@ const firebaseConfig = {
   messagingSenderId: "594807933922",
   appId: "1:594807933922:web:ca6f4b852b5f58bb6147bb",
   measurementId: "G-D4ZER3W4B0"
-};
+});
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase
-const db = firebaseConfig.firestore();
+const db = firebaseApp.firestore();
 
 export { db };
