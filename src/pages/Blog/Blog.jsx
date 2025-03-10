f28be9d91d19e5c1d65434aacd560fe7dcd98832
Aman Shrivastava <aman34503@gmail.com>
2025-03-10 13:21:35 +0530

    add in blog

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,31 +1,36 @@
 // Blog.jsx
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { PageHeader, Nav } from "../../components"; 
 import "./style.css";
 import blogLinks from "./blogLinks.json"; 
 
 const Blog = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  
+    const toggleTheme = () => {
+      setIsDarkTheme(!isDarkTheme);
+    };
   return (
     <motion.div
-      className="page-blog"
+      className={`page-blog ${isDarkTheme ? "dark-theme" : ""}`}
       exit={{ x: "100vw" }}
       transition={{ ease: "easeInOut" }}
     >
-      <Nav />
+      <Nav isDarkTheme={isDarkTheme} toggleTheme={toggleTheme} />
       <div className="blog-container">
       <div style={{marginTop: '92px'}}>
-        <PageHeader />
+        <PageHeader isDarkTheme={isDarkTheme} />
       </div>
 
         <ul className="blog-links">
-        <motion.h3 style={{ marginBottom: '10px', color:'#222222'}}>
+        <motion.h3 style={{ marginBottom: '10px', color:'#222222'}} isDarkTheme={isDarkTheme}>
   blogs.
 </motion.h3>
           {blogLinks.map(blog => (
-            <li key={blog.id}>
-              <a href={blog.link}>{blog.title}</a>
-              <span className="blog-meta">{blog.time} | {blog.location}</span>
+            <li key={blog.id} isDarkTheme={isDarkTheme}>
+              <a href={blog.link}>{blog.title} isDarkTheme={isDarkTheme}</a>
+              <span className="blog-meta">{blog.time} | {blog.location} isDarkTheme={isDarkTheme}</span>
             </li>
           ))}
         </ul>
